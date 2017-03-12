2d472d24c1ed1cdf8adb1334685fd4a1c9154ae5
Roosbeh Almasi <roosbeh@outlook.com>
2017-03-12 12:17:05 +0000

    LPTMR is ounting freely but there seem to be two different overflow interval

--- src/main.c
+++ src/main.c
@@ -33,11 +33,11 @@ int main(void)
 {
 
 
-    SIM->SCGC5 |= 1UL<<10 | 1UL<<13;  // enable clk on port B & E
-    SIM->SCGC5 |= 1UL;     // CLK EN ON LPTMR
+    SIM->SCGC5 |= 1UL<<10 | 1UL<<13 | 1UL;  // enable clk on port B & E & LPTMR
 
-    MCG->C2 &= ~1UL;       // ensure! Select the slow internal 32KHz CLK to MCGIRCLK
-    MCG->C1 |= 1UL<<1;     // ensure! Enable the MCGIRCLK
+
+    MCG->C2 &= ~1UL;                       // ensure! Select the slow internal 32KHz CLK to MCGIRCLK
+    MCG->C1 |= 1UL<<1;                     // ensure! Enable the MCGIRCLK
 
     PORTB->PCR[22] |= 1UL << 8;
     PORTE->PCR[26] |= 1UL<<8;
@@ -46,35 +46,38 @@ int main(void)
     PTB->PSOR |= 1UL<<22;
     PTE->PSOR |= 1UL<<26;
 
-
+/*
     SysTick->CTRL &= ~(1UL);
     SysTick->LOAD = 0xFFFFFF;
     SysTick->VAL = 0;
     SysTick->CTRL |=    (1UL<<2 | 1UL);
 
+*/
 
-    LPTMR0->CSR &= ~1UL;               //Disable the timer first
-    LPTMR0->PSR |= (1UL<<3 | 1UL<<2);  // CLK/4, SOURCE 2
-    LPTMR0->CMR = 40960;
-    LPTMR0->CNR = 40960;
-    //LPTMR0->CSR |=  ~1UL;  // init and run, no IRQ, Pulse counter on rising edge, reset on overflow
+    LPTMR0->CSR = 0;                //Disable the timer first
+    LPTMR0->CMR = 0;
+    LPTMR0->PSR = 0;
+
+    LPTMR0->CSR = (1UL | 1UL<< 2);
 
 
 
     /* TODO - Add your application code here */
     while (1)
     {
-
+/*
     	if((SysTick->CTRL & 1UL<<16) != 0 )
     	  {
     		PTB->PTOR |= 1UL<<22;
     	  }
+*/
 
-    	if(LPTMR0->CMR == LPTMR0->CNR)
+    	LPTMR0->CNR = 1;
+    	if(LPTMR0->CNR  == 65535)
     	  {
 
     		PTE->PTOR |=1UL<<26;
-    		LPTMR0->CSR |= 1UL<< 2;
+
 
     	  }
 
